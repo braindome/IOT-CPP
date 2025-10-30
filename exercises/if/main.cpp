6877d7cda9908cd193dfb15a3c983cbd5cc300ea
Antonio Saracino <antonio.saracino83@gmail.com>
2025-10-30 02:11:58 +0100

    if exercises

--- exercises/if/main.cpp
+++ exercises/if/main.cpp
@@ -4,18 +4,63 @@
 #include <ctime>
 using namespace std;
 
+void if_4() {
+  int age;
+  cout << "Enter age:" << endl;
+  cin >> age;
+  if (age < 18) {
+    cout << "Not adult" << endl;
+  }
+  if (age > 65) {
+    cout << "Retired" << endl;
+  }
+  if (age > 18) {
+    cout << "Adult" << endl;
+  }
+}
+
+void if_3() {
+  float temp;
+  cout << "Enter temperature:" << endl;
+  cin >> temp;
+  if (temp > 38.8) {
+    cout << "You have a fever" << endl;
+  }
+  if (temp > 39.5) {
+    cout << "You should find a doctor" << endl;
+  }
+  if (temp < 37.8) {
+    cout << "You have no fever" << endl;
+  }
+}
+
 void if_1() {
-  int n;
+  int num;
   cout << "Enter a number:";
-  if (n>10) {
+  cin >> num;
+  if (num>10) {
     cout << "Larger than 10";
-  } else if (n<10) {
+  } else if (num<10) {
     cout << "Lesser than 10";
   } else {
     cout << "Is 10";
   }
 }
 
+void if_2() {
+  int n;
+  cout << "How many packages of milk do we have?" << endl;
+  cin >> n;
+  if(n<10) {
+    cout << "Order 30 packages" << endl;
+  } if(n>10 && n<20) {
+    cout << "Order 20 packages" << endl;
+  } else {
+    cout << "We good" << endl;
+  }
+}
+
 int main() {
+  if_4();
 
 }
