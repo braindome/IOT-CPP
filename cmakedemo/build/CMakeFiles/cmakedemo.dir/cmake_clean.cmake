a4b723114e4b11d433212be3fe3e4658b9837ad7
Antonio Saracino <antonio.saracino83@gmail.com>
2025-11-02 21:04:35 +0100

    cmake demo

---
+++ cmakedemo/build/CMakeFiles/cmakedemo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmakedemo.dir/link.d"
+  "CMakeFiles/cmakedemo.dir/main.cpp.o"
+  "CMakeFiles/cmakedemo.dir/main.cpp.o.d"
+  "cmakedemo"
+  "cmakedemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmakedemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
